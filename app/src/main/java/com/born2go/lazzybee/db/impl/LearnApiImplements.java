fa30571a0fd4df1843a7c8dfc465242ef6c808be
NguyenHue <mutang1967@gmail.com>
2015-07-08 17:09:47 +0700

    Add LearnApiImplements.java

--- app/src/main/java/com/born2go/lazzybee/db/impl/LearnApiImplements.java
+++ app/src/main/java/com/born2go/lazzybee/db/impl/LearnApiImplements.java
@@ -122,6 +122,27 @@ public class LearnApiImplements implements LearnApi {
     }
 
     /**
+     * Get Random list card from today
+     *
+     * @param number
+     */
+    @Override
+    public List<Card> _getRandomCard(int number) {
+        return null;
+    }
+
+    /**
+     * export to SqlIte form ListCard
+     *
+     * @param cardList
+     * @return 1 if export complete else 2 to false
+     */
+    @Override
+    public int export(List<Card> cardList) {
+        return 0;
+    }
+
+    /**
      *
      * */
     private List<Card> _getListCard() {
