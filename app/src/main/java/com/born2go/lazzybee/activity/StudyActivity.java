4e05a0d914e2d04c09b8108cb1f9efa821ea3191
NguyenHue <mutang1967@gmail.com>
2015-07-29 10:48:30 +0700

    Function Learn More:Send value learn_more to FragamentStudy.java in StudyActivity.java

--- app/src/main/java/com/born2go/lazzybee/activity/StudyActivity.java
+++ app/src/main/java/com/born2go/lazzybee/activity/StudyActivity.java
@@ -1,19 +1,37 @@
 package com.born2go.lazzybee.activity;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.born2go.lazzybee.R;
 import com.born2go.lazzybee.fragment.FragmentStudy;
+import com.born2go.lazzybee.shared.LazzyBeeShare;
 
-public class StudyActivity extends ActionBarActivity implements  FragmentStudy.FragmentStudyListener {
+public class StudyActivity extends ActionBarActivity implements FragmentStudy.FragmentStudyListener {
 
+    FragmentStudy fragmentStudy;
+    boolean learn_more;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study);
+
+        //int fragment study
+        fragmentStudy = (FragmentStudy) getSupportFragmentManager().findFragmentById(R.id.fragmentStudy);
+
+        //get lean_more form intern
+        learn_more = getIntent().getBooleanExtra(LazzyBeeShare.LEARN_MORE, false);
+//        Bundle bundle=new Bundle();
+//        bundle.putBoolean(LazzyBeeShare.LEARN_MORE, learn_more);
+//        fragmentStudy.setArguments(bundle);
+
+
+    }
+
+    public boolean isLearn_more() {
+        return learn_more;
     }
 
     @Override
