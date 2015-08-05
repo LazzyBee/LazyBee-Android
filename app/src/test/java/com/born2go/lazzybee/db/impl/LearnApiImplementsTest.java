b919938b63e12c0741a3f187f1b1461f4f0c41bc
NguyenHue <mutang1967@gmail.com>
2015-08-05 13:33:44 +0700

    Add and test funciton test_getAnswerHTML() in LearnApiImplementsTest.java

--- app/src/test/java/com/born2go/lazzybee/db/impl/LearnApiImplementsTest.java
+++ app/src/test/java/com/born2go/lazzybee/db/impl/LearnApiImplementsTest.java
@@ -1,18 +1,11 @@
 package com.born2go.lazzybee.db.impl;
 
-import java.lang.System;
-import java.util.Date;
-
-import static org.junit.Assert.*;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.After;
-import org.junit.Test;
-
 import com.born2go.lazzybee.db.Card;
-
 import com.born2go.lazzybee.shared.LazzyBeeShare;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Created by Hue on 7/22/2015.
@@ -44,9 +37,28 @@ public class LearnApiImplementsTest {
 //            System.out.println("Ivl:" + ivl);
 
     }
+
     @Test
     public void test_getListPackageFormString() throws Exception {
         LazzyBeeShare.getListPackageFormString(",common,it,basic,");
 
+    }
+
+    @Test
+    public void test_getAnswerHTML() throws Exception {
+        Card card = new Card();
+        card.setQuestion("card");
+        card.setAnswers("{\"q\":\"smash\"" +
+                ", \"pronoun\":\"/smæʃ/\"" +
+                ", \"packages\":{\"" +
+                "common\":{\"" +
+                "meaning\":\"<p>v. đập n&aacute;t</p>\", \"explain\":\"<p>To <strong>smash</strong> something is to break it into many small pieces</p>\", \"example\":\"<p>Jacob <strong>smashed</strong> the window with a rock</p>\"}}}");
+        card.setLast_ivl(200);
+        card.setDue(38210391803910l);
+        card.setFactor(2000);
+        card.setLevel(5);
+        card.setRev_count(200);
+
+        System.out.println("Card html:" + LazzyBeeShare.getAnswerHTML(card, "menaing", "explain", "example"));
     }
 }
