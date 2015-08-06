4e5d81bb01cac99bcbe90bb662a17796e87c78b7
NguyenHue <mutang1967@gmail.com>
2015-08-06 08:44:01 +0700

    Add test function test_getAnswerHTMLwithPackage() and refactor function test_getAnswerHTML() in LearnApiImplementsTest.java

--- app/src/test/java/com/born2go/lazzybee/db/impl/LearnApiImplementsTest.java
+++ app/src/test/java/com/born2go/lazzybee/db/impl/LearnApiImplementsTest.java
@@ -48,17 +48,38 @@ public class LearnApiImplementsTest {
     public void test_getAnswerHTML() throws Exception {
         Card card = new Card();
         card.setQuestion("card");
-        card.setAnswers("{\"q\":\"smash\"" +
-                ", \"pronoun\":\"/smæʃ/\"" +
-                ", \"packages\":{\"" +
-                "common\":{\"" +
-                "meaning\":\"<p>v. đập n&aacute;t</p>\", \"explain\":\"<p>To <strong>smash</strong> something is to break it into many small pieces</p>\", \"example\":\"<p>Jacob <strong>smashed</strong> the window with a rock</p>\"}}}");
+        card.setAnswers("{\"q\":\"display\", " +
+                "\"pronoun\":\"/dɪˈspleɪ/\", \"packages\":" +
+                "{\"common\":{\"meaning\":\"<p>v. tr&igrave;nh chiếu</p>\"," +
+                " \"explain\":\"<p>To <strong>display</strong> something is to show it, especially by putting it in a certain place</p>\", \"example\":\"<p>The museum <strong>displayed</strong> many wonderful paintings</p>\"}, \"it\":{\"meaning\":\"<p>n. m&agrave;n h&igrave;nh (m&aacute;y t&iacute;nh)</p>\", \"explain\":\"\", \"example\":\"\"}}}");
         card.setLast_ivl(200);
         card.setDue(38210391803910l);
         card.setFactor(2000);
         card.setLevel(5);
         card.setRev_count(200);
 
-        System.out.println("Card html:" + LazzyBeeShare.getAnswerHTML(card, "menaing", "explain", "example"));
+        //System.out.println("Card html:" + LazzyBeeShare.getAnswerHTML(card, "menaing", "explain", "example"));
     }
+
+    @Test
+    public void test_getAnswerHTMLwithPackage() throws Exception {
+        Card card = new Card();
+        card.setQuestion("debate");
+//        card.setAnswers("{\"q\":\"display\", " +
+//                "\"pronoun\":\"/dɪˈspleɪ/\", \"packages\":" +
+//                "{\"common\":{\"meaning\":\"<p>v. tr&igrave;nh chiếu</p>\"," +
+//                " \"explain\":\"<p>To <strong>display</strong> something is to show it, especially by putting it in a certain place</p>\", \"example\":\"<p>The museum <strong>displayed</strong> many wonderful paintings</p>\"}, \"it\":{\"meaning\":\"<p>n. m&agrave;n h&igrave;nh (m&aacute;y t&iacute;nh)</p>\", \"explain\":\"\", \"example\":\"\"}}}");
+        card.setAnswers("{\"q\":\"debate\", \"pronoun\":\"/dɪˈbeɪt/\", \"packages\":{}}");
+
+        card.setLast_ivl(200);
+        card.setDue(38210391803910l);
+        card.setFactor(2000);
+        card.setLevel(5);
+        card.setRev_count(200);
+        LazzyBeeShare.getAnswerHTMLwithPackage(card, "menaing","","",false);
+
+        //System.out.println("Card html:" + );
+    }
+
+
 }
