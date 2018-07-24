07e5eac463e5e1a3b0bb03a29f1d1e2d8997779d
Nguyen Hue <mutang1967@gmail.com>
2018-07-24 11:54:03 +0700

    fix req permission write storage

--- app/src/main/java/com/born2go/lazzybee/activity/SettingActivity.java
+++ app/src/main/java/com/born2go/lazzybee/activity/SettingActivity.java
@@ -1,10 +1,15 @@
 package com.born2go.lazzybee.activity;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.speech.tts.TextToSpeech;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -25,9 +30,16 @@ public class SettingActivity extends AppCompatActivity {
 
     private static final String TAG = "SettingActivity";
     private static final Object GA_SCREEN = "aSettingScreen";
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     Context context;
     RecyclerView mRecyclerViewSettings;
+    private RecyclerViewSettingListAdapter mSettingListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,24 +51,7 @@ public class SettingActivity extends AppCompatActivity {
     }
 
     private void initSettingView() {
-
         mRecyclerViewSettings = (RecyclerView) findViewById(R.id.mRecyclerViewSettings);
-
-        /*context.getString(R.string.setting_learn_title),*//*Title*//*
-                context.getString(R.string.setting_today_new_card_limit),
-                context.getString(R.string.setting_today_review_card_limit),
-                context.getString(R.string.setting_total_learn_per_day),
-                context.getString(R.string.setting_lines),
-                context.getString(R.string.setting_notification),
-                context.getString(R.string.setting_lines),
-                context.getString(R.string.setting_update_title),
-                context.getString(R.string.setting_check_update),
-                context.getString(R.string.setting_auto_check_update),
-                context.getString(R.string.setting_lines),
-                context.getString(R.string.setting_debug_info),
-                context.getString(R.string.setting_lines),
-                context.getString(R.string.setting_language),
-                context.getString(R.string.setting_lines)*/
         final List<String> settings;
         final List<String> devices = Arrays.asList(context.getResources().getStringArray(R.array.devices_dev_id));
         String android_id = Settings.Secure.getString(context.getContentResolver(),
@@ -70,10 +65,10 @@ public class SettingActivity extends AppCompatActivity {
         }
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(context, 1);
-        RecyclerViewSettingListAdapter recyclerViewSettingListAdapter = new RecyclerViewSettingListAdapter(this, context, settings, mRecyclerViewSettings);
+        mSettingListAdapter = new RecyclerViewSettingListAdapter(this, context, settings, mRecyclerViewSettings);
 
         mRecyclerViewSettings.setLayoutManager(gridLayoutManager);
-        mRecyclerViewSettings.setAdapter(recyclerViewSettingListAdapter);
+        mRecyclerViewSettings.setAdapter(mSettingListAdapter);
         setTitle(R.string.action_settings);
     }
 
@@ -104,7 +99,7 @@ public class SettingActivity extends AppCompatActivity {
         try {
             Bundle bundle = new Bundle();
             bundle.putString("screenName", (String) GA_SCREEN);
-            LazzyBeeSingleton.getFirebaseAnalytics().logEvent("screenName",bundle);
+            LazzyBeeSingleton.getFirebaseAnalytics().logEvent("screenName", bundle);
         } catch (Exception e) {
             LazzyBeeShare.showErrorOccurred(context, "_trackerApplication", e);
         }
@@ -146,5 +141,31 @@ public class SettingActivity extends AppCompatActivity {
 
         super.onActivityResult(requestCode, resultCode, data);
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG, "requestCode:" + requestCode + ",permissions:" + Arrays.toString(permissions) + ",grantResults:" + Arrays.toString(grantResults));
+        if (requestCode == REQUEST_EXTERNAL_STORAGE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                mSettingListAdapter.updateRequestPermissions();
+            }
+        }
+    }
+
+    public static boolean verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+            return false;
+
+        } else return true;
     }
 }
