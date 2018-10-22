71cc09ba0d4b7bc6141b9e885fffa0e4c218fb07
Nguyen Hue <mutang1967@gmail.com>
2018-10-22 14:11:22 +0700

    remove file un using

--- app/src/main/java/com/born2go/lazzybee/activity/SettingActivity.java
+++ app/src/main/java/com/born2go/lazzybee/activity/SettingActivity.java
@@ -76,9 +76,6 @@ public class SettingActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_setting, menu);
-
         return true;
     }
 
